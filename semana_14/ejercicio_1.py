114edb10c62da24558ceb04c230004f316c98b5c
frasegura <franciscosegura29@gmail.com>
2024-08-21 22:11:36 -0600

    ejercicio_1 added

---
+++ semana_14/ejercicio_1.py
@@ -0,0 +1,4 @@
+"""1. Cree una estructura de objetos que asemeje un Stack.
+    1. Debe incluir los métodos de `push` (para agregar nodos) y `pop` (para quitar nodos).
+    2. Debe incluir un método para hacer `print` de toda la estructura.
+    3. No se permite el uso de tipos de datos compuestos como `lists`, `dicts` o `tuples` ni módulos como `collections`."""
