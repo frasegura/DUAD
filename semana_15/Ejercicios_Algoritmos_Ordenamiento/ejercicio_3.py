72c1606f8e6a4c0e8071b82135fffa1e61f8c2c1
frasegura <franciscosegura29@gmail.com>
2024-09-23 00:06:58 -0600

    testing ejercicio_3

--- semana_15/Ejercicios_Algoritmos_Ordenamiento/ejercicio_3.py
+++ semana_15/Ejercicios_Algoritmos_Ordenamiento/ejercicio_3.py
@@ -21,8 +21,9 @@ class LinkedList:
     def print_structure(self):
         current_node = self.head
         while current_node is not None:           
-            print(current_node.data)
+            print(current_node.data,end = '->')
             current_node =  current_node.next
+        print('None')
 
     def push(self, new_node):
         if self.head is None:
@@ -41,25 +42,31 @@ class LinkedList:
         while self.head is not None:
             node_change = False
             current_node = self.head
-            while current_node.next is not None: 
-                next_node = current_node.next  
+            print('Empieza de nuevo :  ',current_node.data)
+            #self.print_structure()
+            while current_node.next is not None:   
+                next_node = current_node.next
                 if current_node.data > next_node.data:
                     current_node.data , next_node.data = next_node.data ,current_node.data
                     node_change = True
                 current_node = next_node
+                print('nodo actual :', current_node.data)
+                self.print_structure()
             if not node_change:
                 return
 
-
-
 my_list = LinkedList()
-
 my_list.push(Node(25))
 my_list.push(Node(5))
 my_list.push(Node(1))
 my_list.push(Node(99))
+my_list.push(Node(20))
+my_list.push(Node(89))
+my_list.push(Node(59))
+my_list.push(Node(100))
+print('Lista Original')
 my_list.print_structure()
-
+print('-----')
 my_list.sort_list()
 print('Lista ordenada:')
 my_list.print_structure()
